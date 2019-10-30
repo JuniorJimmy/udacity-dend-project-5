84ca0fb162445dc41b52fc4fdaa9187eb360c3a2
David Rubinger <david.rubinger@gmail.com>
2019-10-30 18:14:44 -0400

    Build and instantiate LoadFactOperator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -8,15 +8,26 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 select_sql="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.select_sql = select_sql
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+
+        self.log.info("Getting credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Loading data into fact table in Redshift")
+        table_insert_sql = f"""
+            INSERT INTO {self.table}
+            {self.select_sql}
+        """
+        redshift_hook.run(table_insert_sql)
